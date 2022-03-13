a36826e9e4772f61605f9df54fbe5b3625d2cbef
Florian Bouron <bouron.florian@yahoo.fr>
2022-03-13 13:07:27 +0100

    Add product overview

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Checkout } from './components/Checkout';
+import ProductView from './components/ProductOverview/ProductOverview';
 
 export default function App() {
-  return <Checkout />;
+  return (
+    <>
+      <ProductView />
+      <Checkout />
+    </>
+  );
 }
